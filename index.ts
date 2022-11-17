602714c21c82dbc3c29c2726da576843dbb2b95c
Xiaoyun <yangx232@gmail.com>
2022-11-17 13:54:52 -0500

    add TS

--- index.ts
+++ index.ts
@@ -1,11 +1,32 @@
-const getNewNodeAddWarning = (id) =>
+// The error message should only come up in development during implementation
+const getNewNodeAddWarning = (id: string) =>
   `cannot add node with id=${id} because a node with this id already already exists in the cache`;
 
-export class MessagesCache {
+interface EndNode<Data> {
+  type: "END_NODE";
+  prev?: Node<Data> | EndNode<Data>;
+  next?: Node<Data> | EndNode<Data>;
+}
+interface Node<Data> {
+  type: "DATA_NODE";
+  prev: Node<Data> | EndNode<Data>;
+  next: Node<Data> | EndNode<Data>;
+  id: string;
+  data: Data;
+}
+
+const isCacheNode = <T>(node: Node<T> | EndNode<T>): node is Node<T> =>
+  node.type === "DATA_NODE";
+
+export class MessagesCache<CacheData> {
+  private cache: Map<string, Node<CacheData>>;
+  private head: EndNode<CacheData>;
+  private tail: EndNode<CacheData>;
+
   constructor() {
     this.cache = new Map();
-    this.head = {};
-    this.tail = {};
+    this.head = { type: "END_NODE" };
+    this.tail = { type: "END_NODE" };
 
     // tail and head are stub nodes
     // so the actual last node is tail.prev
@@ -16,19 +37,16 @@ export class MessagesCache {
     this.tail.prev = this.head;
   }
 
-  /**
-   * Deletes a single message from the cache
-   * @param {string} id
-   * @returns
-   */
-  deleteOne(id) {
+  public deleteOne(id: string) {
+    const node = this.cache.get(id);
+
     // we don't have to return an error to avoid a noisy console
     // the current state of the cache already represent the end
     // state we want the cache to be so there's no additional action
     // for the caller of this method
-    if (!this.cache.get(id)) return;
+    if (!node) return;
 
-    const node = this.cache.get(id);
+    if (!node.prev || !node.next) return;
 
     node.prev.next = node.next;
     node.next.prev = node.prev;
@@ -36,40 +54,37 @@ export class MessagesCache {
     this.cache.delete(id);
   }
 
-  /**
-   *
-   * @param {string} id
-   * @param {*} newData
-   * @returns
-   */
-  updateOne(id, newData) {
-    if (!this.cache.get(id)) {
+  updateOne(id: string, newData: CacheData) {
+    const node = this.cache.get(id);
+    if (!node) {
+      // eslint-disable-next-line no-console
       console.error(
         `cannot update node with id=${id} because it doesn't exist in the cache`
       );
       return;
     }
-    const node = this.cache.get(id);
+
     node.data = newData;
   }
 
-  /**
-   *
-   * @param {string} id
-   * @param {*} data
-   * @returns
-   */
-  addOneAtTail(id, data) {
+  addOneAtTail(id: string, data: CacheData) {
     if (this.cache.get(id)) {
+      // eslint-disable-next-line no-console
       console.error(getNewNodeAddWarning(id));
       return;
     }
     // assumption: what we are adding does not
     // already exist in the cache
     const prevLastNode = this.tail.prev;
-    const newNode = {
+
+    if (!prevLastNode) return;
+
+    const newNode: Node<CacheData> = {
+      type: "DATA_NODE",
       id,
       data,
+      prev: this.tail,
+      next: this.tail,
     };
 
     // make old last node and
@@ -85,34 +100,30 @@ export class MessagesCache {
     this.cache.set(id, newNode);
   }
 
-  /**
-   *
-   * @param {Array} newMessages
-   */
-  addManyAtTail(newMessages) {
-    for (let i = 0; i < newMessages.length; i++) {
+  addManyAtTail(newMessages: Array<{ id: string; data: CacheData }>) {
+    // eslint-disable-next-line no-loops/no-loops
+    for (let i = 0; i < newMessages.length; i += 1) {
       const { id, data } = newMessages[i];
       this.addOneAtTail(id, data);
     }
   }
 
-  /**
-   *
-   * @param {string} id
-   * @param {*} data
-   * @returns
-   */
-  addOneAtHead(id, data) {
+  addOneAtHead(id: string, data: CacheData) {
     if (this.cache.get(id)) {
+      // eslint-disable-next-line no-console
       console.error(getNewNodeAddWarning(id));
       return;
     }
-    const newNode = {
+    const newNode: Node<CacheData> = {
+      type: "DATA_NODE" as const,
       id,
       data,
+      prev: this.tail,
+      next: this.tail,
     };
 
     const prevHead = this.head.next;
+    if (!prevHead) return;
     prevHead.prev = newNode;
     newNode.next = prevHead;
 
@@ -122,25 +133,16 @@ export class MessagesCache {
     this.cache.set(id, newNode);
   }
 
-  /**
-   *
-   * @param {Array} newMessages
-   */
-  addManyAtHead(newMessages) {
-    for (let i = newMessages.length - 1; i >= 0; i--) {
+  addManyAtHead(newMessages: Array<{ id: string; data: CacheData }>) {
+    /* eslint-disable no-loops/no-loops */
+
+    for (let i = newMessages.length - 1; i >= 0; i -= 1) {
       const { id, data } = newMessages[i];
       this.addOneAtHead(id, data);
     }
   }
 
-  /**
-   *
-   * @param {string} oldId
-   * @param {string} newId
-   * @param {*} newData
-   * @returns
-   */
-  replaceOne(oldId, newId, newData) {
+  replaceOne(oldId: string, newId: string, newData: CacheData) {
     const oldNode = this.cache.get(oldId);
 
     // Although in perfect use, we don't expect the API user to call this
@@ -149,20 +151,25 @@ export class MessagesCache {
     // Make our API fault tolerant and not error out if the API user makes
     // mistakes like calling this API with the wrong oldId
     if (!oldNode) {
+      // eslint-disable-next-line no-console
       console.error(
         `cannot replace old node id=${oldId} with new node id=${newId} because the old node doesn't exist in the cache`
       );
       return;
     }
     if (this.cache.has(newId)) {
+      // eslint-disable-next-line no-console
       console.error(
         `cannot replace old node id=${oldId} with new node id=${newId} because the new node already exists in the cache`
       );
       return;
     }
-    const newNode = {
+    const newNode: Node<CacheData> = {
+      type: "DATA_NODE" as const,
       id: newId,
       data: newData,
+      prev: this.tail,
+      next: this.tail,
     };
 
     // make nodes adjacent to oldNode point to newNode instead
@@ -176,30 +183,23 @@ export class MessagesCache {
     this.cache.delete(oldId);
   }
 
-  /**
-   * retrieves the message with the given id from cache
-   * @param {string} id
-   * @returns {*} data
-   */
-  getOne(id) {
+  getOne(id: string) {
     if (!this.cache.has(id)) return null;
 
-    return this.cache.get(id).data;
+    return (this.cache.get(id) as Node<CacheData>).data;
   }
 
-  /**
-   *
-   * @returns {Array<*>} an array of messages in the cache
-   */
   getAll() {
     const res = [];
     let curr = this.head.next;
 
-    while (curr.next) {
+    while (curr && isCacheNode<CacheData>(curr) && curr?.next) {
       // we don't need to worry about handling the null
       // case of getOne because that indicates we have corrupt
       // cache data, which should cause getAll to fail
-      res.push(this.cache.get(curr.id).data);
+
+      res.push(this.cache.get(curr.id)?.data);
+
       curr = curr.next;
     }
 
